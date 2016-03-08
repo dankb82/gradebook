1776abc3d5ee86973526dc4662f60709ad7065c5
Daniel Buompane <danielbuompane@gmail.com>
2016-03-08 14:41:28 -0500

    Generate User controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'user/login'
+
+  get 'user/logout'
+
   resources :teachers
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
