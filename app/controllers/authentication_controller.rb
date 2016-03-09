727891b19dc6c88b9f564ddab395349ad9242b3e
Daniel Buompane <danielbuompane@gmail.com>
2016-03-08 22:51:31 -0500

    Add session to Authentication controller

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -3,6 +3,7 @@ class AuthenticationController < ApplicationController
     if request.post?
       user = Teacher.find_by(email: params[:email])
       if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
         redirect_to teachers_path, notice: "You're logged in."
       else
         flash[:notice] = "Invalid email or password."
