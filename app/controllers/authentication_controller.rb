f5a8118d38845baed22d97760804dab849cd9a26
Daniel Buompane <danielbuompane@gmail.com>
2016-03-09 17:18:51 -0500

    Refactor AuthenticationController create action

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,12 +1,11 @@
 class AuthenticationController < ApplicationController
 
   def create
-    teacher = Teacher.find_by_email(params[:email])
-    if teacher && teacher.authenticate(params[:password])
-      session[:user_id] = teacher.id
-      session[:user_type] = "Teacher"
-
-      redirect_to root_path, notice: "You have succesfully logged in!"
+    user = params[:user_type].constantize.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      session[:user_type] = params[:user_type]
+      redirect_to send("#{params[:user_type].downcase}s_path"), notice: "You have succesfully logged in!"
     else
       flash.now[:alert] = "Login failed: invalid email or password."
       render "new"
@@ -18,5 +17,5 @@ class AuthenticationController < ApplicationController
      session[:user_type] = nil
      redirect_to login_path, notice: "You have logged out."
    end
-   
+
 end
